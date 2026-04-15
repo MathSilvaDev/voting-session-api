69a10b790f1be07b746cf8d10ca840bcc509aa96
Matheus Silva <mathsilva.code@gmail.com>
2026-04-15 14:57:43 -0300

    chore: initial base project

---
+++ src/main/java/com/matheus/voting_session_api/VotingSessionApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.voting_session_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionApiApplication.class, args);
+	}
+
+}
