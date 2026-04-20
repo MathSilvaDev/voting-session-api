61efcb75d060f993ba8a609afb4c9c6ae97054e9
Matheus Silva <mathsilva.code@gmail.com>
2026-04-20 14:22:24 -0300

    feat: add voting session form with datetime inputs and ngModel binding

--- frontend/src/app/voting-session/voting-session.ts
+++ frontend/src/app/voting-session/voting-session.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-voting-session',
-  imports: [],
+  imports: [ FormsModule, CommonModule],
   templateUrl: './voting-session.html',
   styleUrl: './voting-session.scss',
 })
 export class VotingSession {
 
+  topic: string = '';
+  description: string = '';
+  startAt: string = '';
+  endAt: string = '';
+
+  createVotingSession(){}
 }
